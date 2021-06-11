5c2f31b64e218f5257a306586e5bfa42b2034c0d
D-T-1 <66256806+D-T-1@users.noreply.github.com>
2021-06-11 11:17:30 +0200

    Update fdr.R

--- fdr.R
+++ fdr.R
@@ -3,15 +3,10 @@
 
 n<-100
 
-f<-function(x,mu,sigma){
-  const=sigma*sqrt(2*pi)
-  return ((1/const)*exp((-(x-mu)^2)/(2*sigma^2)))
-}
-
 mixture_density <- function(x,weight=c(2,-1),mean=c(2,2),variance=c(2,1)){
   mix <-0
   for(i in 1:length(weight)){
-      mix<-mix + weight[i]*f(x,mean[i],variance[i])
+      mix<-mix + weight[i]*dnorm(x,mean[i],variance[i])
   }
   
   return(mix)
