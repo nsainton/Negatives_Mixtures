e17ad05efcdb3a164aa854a4101b6f67b96f1654
poulem <noah.saintonge@dauphine.eu>
2021-06-19 14:49:04 +0200

    Temporary cleaning of file CleanedZiggurat finally using styler and lintr packages

--- CleanedZiggurat.R
+++ CleanedZiggurat.R
@@ -1,92 +1,52 @@
-rm(list=ls())
-#We begin by creating the first function we need to generate our boxes that will be called boxes.
-#This function we'll take the bounds we need as an input, the number of boxes, the mean and the
-#standard deviation of our normal law and the type of boxes we need (over or under the curve)
-#And will generate two times the number of boxes to approximate the curve with
+rm(list = ls())
+# We begin by creating the first function we need
+# to generate our boxes that will be called boxes.
+# This function we'll take the bounds we need as an input,
+# the number of boxes, the mean and the
+# standard deviation of our normal law and
+# the type of boxes we need (over or under the curve)
+# And will generate two times the number of boxes to approximate the curve with
 
-boxes=function(left_bound=-1,right_bound=1,number_of_boxes=1,mean=0,sd=1,coefficient=1){
-  const=sd*sqrt(2*pi)
-  density=function(x){
-    return ((1/const)*exp((-(x-mean)^2)/(2*sd^2)))
+boxes <- function(inf, sup, number_of_boxes, mean, sd, coef = 1) {
+  bound <- ifelse(abs(mean - inf) > abs(mean - sup), mean + abs(mean - inf), sup)
+  # We're just getting sure that the right bound is the more distant
+  # to the mean of our normal law between the left and the right bound
+  step <- (bound - mean) / number_of_boxes
+  if (coef > 0) {
+    # If the type is "over" we generate one step further to go one step further on the other side by symmetry
+    # We define the intervals accordingly and generate the heights on the giver intervals
+    right_interval <- seq(mean, bound + step, step)
+    left_interval <- sort(-right_interval) + 2 * mean
+    length_right <- length(right_interval) - 1
+    left_interval <- left_interval[1:length_right]
+    right_interval <- right_interval[1:length_right]
+    right_heights <- dnorm(right_interval, mean, sd)
+    left_heights <- sort(right_heights)[1:length_right]
+  } else if (coef < 0) {
+    # If the type is "under" we also generate one step further in order to translate it of one step
+    # we generate and cut our intervals at the end of the generation
+    right_interval <- seq(mean, bound + step, step)
+    right_heights <- dnorm(right_interval, mean, sd)[1:length(right_interval)]
+    left_heights <- sort(right_heights)[1:length(right_heights) - 1]
+    right_heights <- right_heights[2:length(right_heights)]
+    right_interval <- right_interval[1:length(right_interval) - 1]
+    left_interval <- sort(-right_interval) + 2 * mean - step
   }
-  bound = ifelse(abs(mean-left_bound)>abs(mean-right_bound),mean+abs(mean-left_bound),right_bound)
-  #print(bound)
-  #We're just getting sure that the right bound is the more distant to the mean of our normal law between the left and the right bound
-  step=(bound-mean)/number_of_boxes
-  if(coefficient>0){
-    #If the type is "over" we generate one step further to go one step further on the other side by symmetry
-    #We define the intervals accordingly and generate the heights on the giver intervals
-    right_interval=seq(mean,bound+step,step)
-    left_interval=sort(-right_interval)+2*mean
-    length_right = length(right_interval)-1
-    left_interval=left_interval[1:length_right]
-    right_interval=right_interval[1:length_right]
-    right_heights=density(right_interval)
-    left_heights=sort(right_heights)[1:length_right]
-  }else if(coefficient<0){
-    #If the type is "under" we also generate one step further in order to translate it of one step
-    #we generate and cut our intervals at the end of the generation
-    right_interval=seq(mean,bound+step,step)
-    right_heights=density(right_interval)[1:length(right_interval)]
-    left_heights=sort(right_heights)[1:length(right_heights)-1]
-    right_heights=right_heights[2:length(right_heights)]
-    right_interval=right_interval[1:length(right_interval)-1]
-    left_interval=sort(-right_interval)+2*mean-step
-  }
-  intervals=c(left_interval,right_interval)
-  heights=c(left_heights,right_heights)
-  return(list(intervals,coefficient*heights))
+  intervals <- c(left_interval, right_interval)
+  heights <- c(left_heights, right_heights)
+  heights <- coef * heights
+  return(list("intervals" = intervals, "heights" = heights))
 }
 
-generate_boxes = function(weights,means,standard_deviations,numberofboxes=5){
-  lb = qnorm(0.001,min(means),max(standard_deviations))
-  rb = qnorm(0.999,max(means),max(standard_deviations))
-  size = length(weights)
-  list_of_normal_boxes = lapply(1:size, function(i){
-    boxes(left_bound = lb, right_bound = rb, number_of_boxes = numberofboxes, mean=means[i], sd=standard_deviations[i],coefficient = weights[i])
-  })
-  list_of_dots = lapply(1:size, function(i) list_of_normal_boxes[[i]][[1]])
-  list_of_heights = lapply(1:size, function(i) list_of_normal_boxes[[i]][[2]])
-  #In these first lines, we generate the boxes for all the normal laws of the mixture and extract the corresponding heights and dots
-  size_list = length(list_of_dots[[1]])
-  indexed_dots = lapply(1:size, function(i){
-    list_of_dots[[i]] = lapply(1:size_list, function(j){
-      c(list_of_dots[[i]][[j]],round(i,0),round(j,0))
-    })
-  })
-  indexed_heights = lapply(1:size, function(i){
-    list_of_heights[[i]] = lapply(1:size_list, function(j){
-      c(list_of_heights[[i]][[j]],round(i,0),round(j,0))
-    })
-  })
-  #In these two variables we indice the values with two number. The first one represents the position of the normal law in the mixture
-  #and the second one represents the position of the value in the list
-  dots = Reduce(c,indexed_dots)
-  dots = dots[order(sapply(dots,function(x) x[1],simplify = TRUE))]
-  #We now put all the indexed dots in a unique list and are going to put all the summed heights in another one
-  count = rep(1,size_list)
-  heights = lapply(1:length(dots),function(i){
-    if(i==1){
-      summ=sapply(1:size,function(j) indexed_heights[[i]][[1]],simplify = TRUE)
-      sum(summ)
-    }else{
-      summ=sapply(1:size, function(j){
-        if(dots[[i]][[2]]==j) c=1 #empty line just to not forget to continue there
-      })
-    }
-  })
-  indexed_heights
-}
-
-moyenne=12
-std=5
-weights=c(2,-1)
-means=c(2,3)
-standard_deviations=c(2,5)
-coef=4
-a=boxes(mean=moyenne,number_of_boxes = 20,sd=std,coefficient=coef)
-x=a[[1]]
-y=a[[2]]
-plot(x,y,type='s')
-lines(x,coef*dnorm(x,mean=moyenne,sd=std))
-b=generate_boxes(weights,means,standard_deviations)
+moyenne <- 12
+std <- 5
+weights <- c(2, -1)
+means <- c(2, 3)
+standard_deviations <- c(2, 5)
+co <- 4
+a <- boxes(inf = -1, sup = 1, mean = moyenne, number_of_boxes = 20, sd = std, coef = co)
+x <- a$intervals
+y <- a$heights
+plot(x, y, type = "s")
+lines(x, co * dnorm(x, mean = moyenne, sd = std))
+# b=generate_boxes(weights,means,standard_deviations)
