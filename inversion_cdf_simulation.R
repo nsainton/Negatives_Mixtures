cb8873b11b59965ee3960182586554c75e29c1a7
ClementChauvet <83658432+ClementChauvet@users.noreply.github.com>
2021-07-06 19:14:48 +0200

    Add files via upload

--- inversion_cdf_simulation.R
+++ inversion_cdf_simulation.R
@@ -5,10 +5,47 @@ cdf <- function(x, w, mean, sd) {
   }
   return(res)
 }
-invcdf <- function(q, w, mean, sd, n=1, n_points=100) {
-  y <- seq(min(mean - 3 * sd), max(mean + 3 * sd), length.out = n_points)
-  x <- cdf(y, w, mean, sd)
-  inv <- approxfun(x, y, yleft = 0, yright = 1, method = "linear")
+
+invcdf <- function(w, mean, sd, n=1, n_points=100, tol=0.0001) {
+  x <- seq(min(mean - 3 * sd), max(mean + 3 * sd), length.out = n_points)
+  y <- cdf(x, w, mean, sd)
   u <- runif(n)
-  return(inv(u))
+  res <- rep(0, n)
+  for (i in 1:n) {
+    index_sup <- Position(function(x) x > u[i], y)
+    if (is.na(index_sup)) {
+      #Search y superior to u[i]
+      append(x, qnorm((u[i] + 1) / 2, max(mean), max(sd)))
+      append(y, cdf(tail(x, 1), w, mean, sd))
+      inf <- x[length(x) - 1]
+      sup <- x[length(x)]
+      index_sup <- length(x)
+    }
+    else if (index_sup == 1) {
+      #Search y inferior to u[i]
+      x <- c(qnorm(u[i] / 2, min(mean), max(sd)), x)
+      y <- c(cdf(x[1], w, mean, sd), y)
+      inf <- x[1]
+      sup <- x[2]
+      index_sup <- 2
+    }
+    else{
+      inf <- x[index_sup - 1]
+      sup <- x[index_sup]
+    }
+    x_n <- (inf + sup) / 2
+    y_n <- cdf(x_n, w, mean, sd)
+    while (abs(y_n - u[i]) > tol) {
+      if (y_n < u[i]) {
+        inf <- x_n
+      }
+      else{
+        sup <- x_n
+      }
+      x_n <- (inf  + sup) / 2
+      y_n <- cdf(x_n, w, mean, sd)
+    }
+    res[i] <- x_n
+  }
+  return(res)
 }
