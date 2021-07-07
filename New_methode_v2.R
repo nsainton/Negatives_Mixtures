533f645b105c3afdce634ac6688389b1e4392728
D-T-1 <66256806+D-T-1@users.noreply.github.com>
2021-07-07 14:01:26 +0200

    Update New_methode_v2.R

--- New_methode_v2.R
+++ New_methode_v2.R
@@ -6,7 +6,7 @@ n <- 100
 w <- c(2, -1)
 mean <- c(2, 2)
 sd <- c(2, 1)
- 
+
 cumu <- function(x, w=c(2, -1), mean=c(2, 2), sd=c(2, 1)) {
   cumulative <- 0
   for (i in 1:length(mean)) {
@@ -17,9 +17,9 @@ cumu <- function(x, w=c(2, -1), mean=c(2, 2), sd=c(2, 1)) {
 
 #Calcul de la fdr
 fdr <- function(n=100, w=c(2, -1), mean=c(2, 2), sd=c(2, 1)) {
-  r1 <- qnorm(0.001, min(mean), max(sd))
-  r2 <- qnorm(0.999, max(mean), max(sd))
-  K <- seq(r1, r2, (abs(r1 - r2) / n))
+  r_1 <- qnorm(0.001, min(mean), max(sd))
+  r_2 <- qnorm(0.999, max(mean), max(sd))
+  K <- seq(r_1, r_2, (abs(r_1 - r_2) / n))
   cumulate <- matrix(nrow = 1, ncol = length(K))
   cumulate <- lapply(K, cumu)
   return(list(cumulate, K))
@@ -32,40 +32,30 @@ mixture <- function(x) {
   Reduce("+", normal_laws)
 }
 
-choice_h <- function(i) {
-  x <- mixture(L[i])
-  y <- mixture(L[i + 1])
-  if (x > y) {
-    return(list(x, y))
-  }
-  else (return(list(y, x)))
-}
-
-
-L <- fdr(n, w, mean, sd)[[2]]
+Liste <- fdr(n, w, mean, sd)[[2]]
 cumulate <- fdr(n, w, mean, sd)[[1]]
 
 simulate <- function(n = 1000) {
   
-  U1 <- runif(n)
-  nbAGenerer <- rep(0, length(L))
+  U_1 <- runif(n)
+  nbAGenerer <- rep(0, length(Liste))
   nbGenere <- 0
   res <- rep(0, n)
   for (i in 1:(length(cumulate) - 1)) {
     nbGenere <- 0
-    #Choisis selon quel rectangle de la fdr on va gÈnÈrer
-    AGenerer <- which(U1 < cumulate[i + 1] & U1 >= cumulate[i])
+    #Choisis selon quel rectangle de la fdr on va g√©n√©rer
+    AGenerer <- which(U_1 < cumulate[i + 1] & U_1 >= cumulate[i])
     nbAGenerer[i] <- length(AGenerer)
     #Acceptation rejet
-    while(nbGenere < nbAGenerer[i]) {
+    while (nbGenere < nbAGenerer[i]) {
       #Une fois que l'on sait dans quel rectangle tirer Y, C'est une loi uniforme entre 2 points
-      Y <- runif((nbAGenerer[i] - nbGenere), min = L[i], max = L[i + 1])
-      U2 <- runif((nbAGenerer[i] - nbGenere))
-      height <- choice_h(i)[[1]]
-      Acceptes <- which((height * U2) <= mixture(Y))
+      Y <- runif((nbAGenerer[i] - nbGenere), min = Liste[i], max = Liste[i + 1])
+      U_2 <- runif((nbAGenerer[i] - nbGenere))
+      height <- max(mixture(Liste[i]), mixture(Liste[i + 1]))
+      Acceptes <- which((height * U_2) <= mixture(Y))
       if (length(Acceptes) > 0) {
-        res[AGenerer[(nbGenere+1):min((nbGenere+length(Acceptes)),nbAGenerer[i])]]=Y[Acceptes[1:min(nbAGenerer[i],length(Acceptes))]]
-        nbGenere=nbGenere+length(Acceptes)
+        res[AGenerer[(nbGenere + 1):min((nbGenere + length(Acceptes)), nbAGenerer[i])]]=Y[Acceptes[1 : min(nbAGenerer[i], length(Acceptes))]]
+        nbGenere=nbGenere + length(Acceptes)
       }
     }
   }
@@ -73,7 +63,8 @@ simulate <- function(n = 1000) {
 }
 
 res <- simulate(100)
-p1 <- hist(main = "RÈpartition des valeurs gÈnÈrÈes", xlab = "Valeurs", ylab = "RÈpartition", res, breaks = 100, freq = FALSE)
+p1 <- hist(main = "R√©partition des valeurs g√©n√©r√©es", xlab = "Valeurs", ylab = "R√©partition", res, breaks = 100, freq = FALSE)
 curve(2 * dnorm(x, 2, 2) - dnorm(x, 2, 1), add = TRUE, col = "red")
 
-microbenchmark::microbenchmark(simulate(1000))
+microbenchmark::microbenchmark(simulate(100000))
+
