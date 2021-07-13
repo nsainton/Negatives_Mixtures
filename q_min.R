a7fe9ae44057e3d6560c08d2096d9078fc479d4a
ClementChauvet <83658432+ClementChauvet@users.noreply.github.com>
2021-07-13 19:49:04 +0200

    Add files via upload

--- q_min.R
+++ q_min.R
@@ -14,3 +14,19 @@ q_min <- function(q, w, means, sd, left_x, left_y, second_left_x, second_left_y)
   }
   return(c(xres, yres))
 }
+
+q_max <-  function(q, w, means, sd, right_x, right_y, second_right_x, second_right_y) {
+  m <- (right_y - second_right_y) / (right_x - second_right_x)
+  b <- right_y - m * right_x
+  xres <- (1 - b) / m
+  yres <- pmixture(xres, w, means, sd)
+  while (q > yres) {
+    second_right_x <- xres
+    second_right_y <- yres
+    m <- (right_y - second_right_y) / (right_x - second_right_x)
+    b <- right_y - m * right_x
+    xres <- (1-b) / m
+    yres <- pmixture(xres, w, means, sd)
+  }
+  return(c(xres, yres))
+}
